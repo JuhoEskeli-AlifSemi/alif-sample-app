699f38d928fcdb147249d1de054c049414a22716
Juho Eskeli <juho.eskeli@alifsemi.com>
2025-11-28 11:01:54 +0200

    OSPI flash examples for HE&HP

--- project/apps/he_app/sysbuild.cmake
+++
@@ -1,2 +0,0 @@
-# Include MCUboot base configuration
-set(mcuboot_EXTRA_CONF_FILE "${CMAKE_CURRENT_LIST_DIR}/../../../bootloader/mcuboot/boot/zephyr/prj.conf" CACHE STRING "MCUboot base configuration" FORCE)
